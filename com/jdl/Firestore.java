8bce6e2fc23a122919a04afc495fdfc2adf7780a
Oseamiya <85782575+oseamiya@users.noreply.github.com>
2021-11-13 19:50:34 +0545

    Updated to AndroidX

--- com/jdl/Firestore.java
+++ com/jdl/Firestore.java
@@ -2,9 +2,9 @@ package com.jdl.Firestore;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.WorkerThread;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
 import android.util.Log;
 
 import com.google.android.gms.common.internal.MetadataValueReader;
